fba91e72920b3ace8e3991715bbf86b401b70c5d
Michael Fischer <michaelandrewfischer@gmail.com>
2021-02-12 00:22:41 -0600

    use v-data-table for logs

--- src/logger/index.js
+++ src/logger/index.js
@@ -5,14 +5,6 @@ import path from 'path';
 function getLogger(logDirectory) {
   const transport = [
     new DailyRotateFile({
-      filename: path.resolve(logDirectory, 'error-%DATE%.log'),
-      level: 'error',
-      datePattern: 'YYYY-MM-DD',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-    }),
-    new DailyRotateFile({
       filename: path.resolve(logDirectory, 'info-%DATE%.log'),
       datePattern: 'YYYY-MM-DD',
       zippedArchive: true,
@@ -33,10 +25,10 @@ function getLogger(logDirectory) {
   }
 
   return createLogger({
-    level: 'info',
+    level: 'silly',
     format: format.combine(
       format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss',
+        format: 'YYYY-MM-DD HH:mm:ss.SSS',
       }),
       // wating on https://github.com/winstonjs/winston/issues/1724, see also https://github.com/winstonjs/logform/pull/106
       // format.errors({ stack: true }),
@@ -49,72 +41,3 @@ function getLogger(logDirectory) {
 }
 
 export default getLogger;
-
-//
-// If we're not in production then **ALSO** log to the `console`
-// with the colorized simple format.
-//
-
-// ***************
-// Allows for JSON logging
-// ***************
-
-// logger.log({
-//   level: 'info',
-//   message: 'Pass an object and this works',
-//   additional: 'properties',
-//   are: 'passed along',
-// });
-
-// logger.info({
-//   message: 'Use a helper method if you want',
-//   additional: 'properties',
-//   are: 'passed along',
-// });
-
-// ***************
-// Allows for parameter-based logging
-// ***************
-
-// logger.log('info', 'Pass a message and this works', {
-//   additional: 'properties',
-//   are: 'passed along',
-// });
-
-// logger.info('Use a helper method if you want', {
-//   additional: 'properties',
-//   are: 'passed along',
-// });
-
-// ***************
-// Allows for string interpolation
-// ***************
-
-// info: test message my string {}
-// logger.log('info', 'test message %s', 'my string');
-
-// info: test message my 123 {}
-// logger.log('info', 'test message %d', 123);
-
-// info: test message first second {number: 123}
-// // logger.log('info', 'test message %s, %s', 'first', 'second', { number: 123 });
-
-// // prints "Found error at %s"
-// logger.info('Found %s at %s', 'error', new Date());
-// logger.info('Found %s at %s', 'error', new Error('chill winston'));
-// logger.info('Found %s at %s', 'error', /WUT/);
-// logger.info('Found %s at %s', 'error', true);
-// logger.info('Found %s at %s', 'error', 100.00);
-// logger.info('Found %s at %s', 'error', ['1, 2, 3']);
-
-// ***************
-// Allows for logging Error instances
-// ***************
-
-// logger.warn(new Error('Error passed as info'));
-// logger.log('error', new Error('Error passed as message'));
-
-// logger.warn('Maybe important error: ', new Error('Error passed as meta'));
-// logger.log('error', 'Important error: ', new Error('Error passed as meta'));
-
-// logger.error(new Error('Error as info'));
--- src/views/Logs.vue
+++ src/views/Logs.vue
@@ -1,19 +1,11 @@
 <template>
   <div class="logs">
-    <div v-for="(log, index) in logs" :key="index">
-      <div v-if="log.level === 'info'">
-        <span class="font-weight-bold">{{log.timestamp}}:  </span>
-        <span class="green--text">{{log.message}}</span>
-      </div>
-      <div v-else-if="log.level === 'error'">
-        <span class="font-weight-bold">{{log.timestamp}}:  </span>
-        <span class="red--text font-italic">{{log.message}}</span>
-      </div>
-      <div v-else>
-        <span class="font-weight-bold">{{log.timestamp}}:  </span>
-        <span class="blue--text">{{log.message}}</span>
-      </div>
-    </div>
+    <v-data-table
+      :headers="headers"
+      :items="logs"
+      class="elevation-1"
+    >
+    </v-data-table>
   </div>
 </template>
 
@@ -26,22 +18,51 @@ import { DateTime } from 'luxon';
 
 export default {
   name: 'Logs',
+  methods: {
+    fetchLogs() {
+      const self = this;
+      self.logs = [];
+      const readInterface = readline.createInterface({
+        input: fs.createReadStream(path.resolve(self.$store.state.home, `info-${DateTime.local().toFormat('yyyy-MM-dd')}.log`)),
+        output: process.stdout,
+        console: false,
+      });
+      readInterface.on('line', (line) => {
+        self.logs.push(JSON.parse(line));
+      });
+    },
+  },
   data() {
     return {
-      logs: null,
+      logInterval: null,
+      logs: [],
+      headers: [{
+        text: 'Level',
+        align: 'start',
+        value: 'level',
+      },
+      {
+        text: 'Timestamp',
+        value: 'timestamp',
+      },
+      {
+        text: 'Message',
+        value: 'message',
+      }],
     };
   },
-  mounted() {
+  created() {
     const self = this;
-    self.logs = [];
-    const readInterface = readline.createInterface({
-      input: fs.createReadStream(path.resolve(self.$store.state.home, `info-${DateTime.local().toFormat('yyyy-MM-dd')}.log`)),
-      output: process.stdout,
-      console: false,
-    });
-    readInterface.on('line', (line) => {
-      self.logs.push(JSON.parse(line));
-    });
+    self.fetchLogs();
+    self.logInterval = setInterval(() => {
+      self.fetchLogs();
+      console.log('fetching');
+    }, 5000);
+  },
+  beforeDestroy() {
+    if (this.logInterval) {
+      clearInterval(this.logInterval);
+    }
   },
 };
 </script>
