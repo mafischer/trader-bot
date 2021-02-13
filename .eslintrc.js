eb8f34e68e3c9b3ed75e3ccc0250c05c4efbb7e7
Michael Fischer <michaelandrewfischer@gmail.com>
2021-02-12 20:29:10 -0600

    change to windows linebreaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
+    'linebreak-style': ["error", "windows"],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'import/no-extraneous-dependencies': ['error', {'devDependencies': true}],
